19211ffa36021b5db28e1c5a76a4cc0e72dcc4f7
zaddhuinfotech <zaddhuinfotech@gmail.com>
2026-07-31 17:23:46 +0530

    ContactForm.jsx
    Inside a character class ([]) the dot does not need escaping.

--- src/components/common/ContactForm.jsx
+++ src/components/common/ContactForm.jsx
@@ -127,7 +127,7 @@ const ContactForm = () => {
                 {...register("email", {
                   required: "Email is required",
                   pattern: {
-                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                    value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/,
                     message: "Entered email is invalid",
                   },
                 })}
